d37d13829eef5217474400497a3a1d19762206d4
kanegon <kanegon9365@gmail.com>
2019-01-03 22:42:52 +0900

    ログイン機能さくせい

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,20 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attribute(user_params)
       redirect_to @user
     else
       render 'new'
