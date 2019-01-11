1a96af364c57e3202a77e6b6a5ecc20c698fd3c0
kanegon <kanegon9365@gmail.com>
2019-01-11 19:16:53 +0900

    ログインアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+
+  before_action :logged_in_user, only:[:index, :edit, :update]
+  before_action :correct_user, only:[:edit, :update]
+
   def new
     @user = User.new(params[:id])
   end
@@ -19,7 +23,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update_attribute(user_params)
+    if @user.update_attributes(user_params)
       redirect_to @user
     else
       render 'new'
@@ -30,9 +34,27 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def index
+    @users = User.all
+  end
+
   private
   
     def user_params
       params.require(:user).permit(:name, :email, :password,:password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        store_location
+        redirect_to login_path
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      unless current_user?(@user)
+        redirect_to edit_user_path(current_user)
+      end
     end
 end
